f0642c64a4e183941b73892b109746b42dfdf301
Rubesh Kumar.R <rubeshkumar135@gmail.com>
2026-03-19 23:59:16 +0530

    profile update on middleware

--- Middlewares/uploadMiddleware.js
+++ Middlewares/uploadMiddleware.js
@@ -1,15 +1,26 @@
 import multer from "multer";
-import multerStorageCloudinary from "multer-storage-cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 import cloudinary from "../Config/cloudinary.js";
-
-const { CloudinaryStorage } = multerStorageCloudinary;
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
-    folder: "restaurants",
-    allowedFormats: ["jpg", "jpeg", "png"]
-  }
+    folder: "profileImages",
+
+    // 🔥 FILE FORMAT CONTROL
+    format: async (req, file) => {
+      const ext = file.mimetype.split("/")[1]; // jpg/png/jpeg
+
+      if (!["jpg", "jpeg", "png"].includes(ext)) {
+        throw new Error("Only JPG, JPEG, PNG allowed");
+      }
+
+      return ext; // keep original format
+    },
+
+    public_id: (req, file) =>
+      Date.now() + "-" + file.originalname,
+  },
 });
 
 const upload = multer({ storage });
